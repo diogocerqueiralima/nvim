83ae80983de2b68a493acb1eba0a8050f2968c25
Diogo Lima <diogocerqueiradelima@gmail.com>
2026-07-23 20:45:07 +0100

    feat: add neovim options

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
+vim.g.mapleader 	= " "
+vim.g.maplocalleader	= " "
+
 require("config.options")
-require("config.keymap")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,1 +1,12 @@
-vim.opt.tabstop	= 4
+vim.o.number		= true			-- enable line number
+
+vim.o.clipboard		= "unnamedplus"	-- sync clipboard between OS and Nvim
+
+vim.o.undofile		= true			-- enable undo/redo changes even after close and reopen the file
+
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.o.cursorline	= true			-- show which line your cursor is
+
+vim.o.scrolloff		= 10			-- minimal number of screen lines to keep above and below the cursor
