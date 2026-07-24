1e660a4ef6cc9006091510de8ac850dc2002a130
Diogo Lima <diogocerqueiradelima@gmail.com>
2026-07-24 21:29:31 +0100

    feat: add nvim-tree

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -16,3 +16,7 @@ vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Live grep" })
 vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Buffers" })
 vim.keymap.set("n", "<leader>gd", telescope.lsp_definitions, { desc = "Goto definition" })
 vim.keymap.set("n", "<leader>gi", telescope.lsp_implementations, { desc = "Goto implementation" })
+
+-- NVIM TREE
+
+vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
